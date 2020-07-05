a594d7face99ff59f2ed7e49918ed4051178e908
Shreyans Jain <jainshreyans121@gmail.com>
2020-07-06 03:35:11 +0530

    Dict(inc)

---
+++ Recursion/DictSmaller.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void dictSmall(string sort, string str, int i, int j) {
+  if(str < sort) {
+    cout<<sort;
+    return zero;
+  }
+}
+
+int main() {
+  string str;
+  cin>>str;
+
+
+}
